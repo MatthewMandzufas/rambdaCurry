2491605cc8a0e607021230e525a172b51397adc4
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-03-22 16:57:13 +0800

    RED/GREEN: Curry > curries multiple values

--- src/curry.test.js
+++ src/curry.test.js
@@ -18,4 +18,15 @@ describe('Curry', () => {
         expect(g(6, 2)).toBe(15);
         expect(mockedFunctionToCurry).toHaveBeenCalledWith(12, 3, 6, 2);
     });
+    it('allows further currying of a curried function', () => {
+        const mockedFunctionToCurry = jest.fn(function (a, b, c, d) {
+            return (a + b * c) / d;
+        });
+        const f = curry(mockedFunctionToCurry);
+        const g = f(12);
+        const h = g(3);
+
+        expect(h(6, 2)).toBe(15);
+        expect(mockedFunctionToCurry).toHaveBeenCalledWith(12, 3, 6, 2);
+    });
 });
