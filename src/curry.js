a7f66d8c79f1dce5b541bc33ed11b136302d9663
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-03-22 17:03:16 +0800

    RED: Curry › properly reports the length of the curried function

--- src/curry.js
+++ src/curry.js
@@ -1,19 +1,61 @@
+function functionOfArity(fn, arity) {
+    if (arity == 4) {
+        return (a, b, c, d) => {
+            return fn(a, b, c, d);
+        };
+    } else if (arity == 3) {
+        return (a, b, c) => {
+            return fn(a, b, c);
+        };
+    } else if (arity == 2) {
+        return (a, b) => {
+            return fn(a, b);
+        };
+    } else if (arity == 1) {
+        return (a) => {
+            return fn(a);
+        };
+    } else {
+        return fn();
+    }
+}
+
 function curry(func) {
     let providedArguments = [];
     const numberOfRequiredArguments = func.length;
 
     function curriedFunction(...args) {
         providedArguments = [...providedArguments, ...args];
+        providedArguments = providedArguments.filter((argument) => {
+            return argument !== undefined;
+        });
         const currentNumberOfArguments = providedArguments.length;
-
         const allArgumentsProvided =
-            currentNumberOfArguments == numberOfRequiredArguments;
-
+            currentNumberOfArguments === numberOfRequiredArguments;
+        console.log('Called with args: ' + args);
+        console.log('Total Provided Args: ' + providedArguments);
+        console.log('All arguments provided?: ' + allArgumentsProvided);
+        console.log(
+            'Number of required: ' +
+                (numberOfRequiredArguments - currentNumberOfArguments)
+        );
         if (allArgumentsProvided) {
             return func(...providedArguments);
         }
-        return curriedFunction;
+        return functionOfArity(
+            curriedFunction,
+            numberOfRequiredArguments - currentNumberOfArguments
+        );
     }
-    return curriedFunction;
+    return functionOfArity(curriedFunction, numberOfRequiredArguments);
 }
+// function curry(fn, arity = fn.length) {
+//     return function curried(...args) {
+//         if (args.length >= arity) {
+//             return fn(...args);
+//         } else {
+//             return (...moreArgs) => curried(...args, ...moreArgs);
+//         }
+//     };
+// }
 export default curry;
--- src/curry.test.js
+++ src/curry.test.js
@@ -29,4 +29,21 @@ describe('Curry', () => {
         expect(h(6, 2)).toBe(15);
         expect(mockedFunctionToCurry).toHaveBeenCalledWith(12, 3, 6, 2);
     });
+    it('properly reports the length of the curried function', () => {
+        const mockedFunctionToCurry = jest.fn(function (a, b, c, d) {
+            return (a + b * c) / d;
+        });
+        const f = curry(mockedFunctionToCurry);
+        expect(f.length).toEqual(4);
+
+        const g = f(12);
+        expect(g.length).toEqual(3);
+
+        const h = g(3);
+        expect(h.length).toEqual(2);
+
+        expect(g.length).toEqual(3);
+        expect(g(3, 6).length).toEqual(1);
+        // TODO: Giving undefined, (this is assumed correct if all arguments for the original argument are provided). The issue here being the provided arguments end up being [12, 3, 3, 6], but it should be [12, 3, 6, undefined]
+    });
 });
